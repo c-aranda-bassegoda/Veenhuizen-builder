96d741a63d7d8b6e19165ccb87a706919441f173
c-aranda-bassegoda <carolina.aranda.2699@gmail.com>
2025-11-04 15:31:49 +0100

    Hover information

---
+++ Assets/Scripts/Tooltip.cs
@@ -0,0 +1,61 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class Tooltip : MonoBehaviour
+{
+    public TextMeshProUGUI headerField;
+    public TextMeshProUGUI bodyField;
+    public LayoutElement layoutElement;
+    public int characterWrapLimit;
+
+    public RectTransform rectTransform;
+    public float offset = 100f;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void SetText(string body, string header = "")
+    {
+        if (string.IsNullOrEmpty(header))
+        {
+            headerField.gameObject.SetActive(false);
+        }
+        else
+        {
+            headerField.gameObject.SetActive(true);
+            headerField.text = header;
+        }
+        bodyField.text = body;
+        
+        int headerLength = headerField.text.Length;
+        int bodyLength = bodyField.text.Length;
+
+        layoutElement.enabled = (headerLength > characterWrapLimit || bodyLength > characterWrapLimit);
+    }
+    void Update()
+    {
+        if (Application.isEditor)
+        {
+            int headerLength = headerField.text.Length;
+            int bodyLength = bodyField.text.Length;
+
+            layoutElement.enabled = (headerLength > characterWrapLimit ||  bodyLength > characterWrapLimit); 
+        }
+
+        Vector2 position = Input.mousePosition;
+
+        float pivotX = (position.x) / Screen.width;
+        float pivotY = (position.y) / Screen.height;
+        if (pivotY >= 0)
+            pivotY += (offset / Screen.height);
+        else 
+            pivotY -= (offset / Screen.height);
+
+        rectTransform.pivot = new Vector2(pivotX, pivotY);
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/TooltipSystem.cs
@@ -0,0 +1,22 @@
+using UnityEditor.Search;
+using UnityEngine;
+
+public class TooltipSystem : MonoBehaviour
+{
+    private static TooltipSystem current;
+    public Tooltip tooltip;
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public static void Show(string body, string header = "")
+    {
+        current.tooltip.SetText(body, header);
+        current.tooltip.gameObject.SetActive(true);
+    }
+    public static void Hide()
+    {
+        current.tooltip.gameObject.SetActive(false);
+    }
+}
