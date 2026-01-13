f3b21868ac3232b205ee3f869a8c690641eea7fc
Pepijn <pepijn.luchtmeijer@gmail.com>
2026-01-13 17:28:49 +0100

    somehow fixed preview thing

--- Assets/Scripts/PlacedObject.cs
+++ Assets/Scripts/PlacedObject.cs
@@ -202,9 +202,8 @@ public class PlacedObject : MonoBehaviour
             exclamationMark.SetActive(true);
         }
         Debug.Log("building start");
-        
-        if(isModule) buildingOrigin = transform;
-        else buildingOrigin = transform.GetChild(0);
+
+        buildingOrigin = transform.GetChild(0);
 
         if (gameObject.tag == "Farmland") buildingOrigin.rotation = Quaternion.Euler(0, 90, 0);
         else if (gameObject.tag != "Road") buildingOrigin.rotation = Quaternion.Euler(0, GetScriptableObject().GetRotationAngle(dir), 0);
