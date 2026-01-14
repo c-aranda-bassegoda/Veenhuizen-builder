de2f8a78775a4a45512f91453d40093e3710dc79
c-aranda-bassegoda <carolina.aranda.2699@gmail.com>
2026-01-14 11:19:41 +0100

    Season manager

--- Assets/Scripts/EconomyManager.cs
+++ Assets/Scripts/EconomyManager.cs
@@ -28,10 +28,11 @@ public class EconomyManager : MonoBehaviour
     //[SerializeField] private List<BuildingScriptableObject> buildings;
     [SerializeField] private List<BuildingScriptableObject> placedBuildingsSOs;
     [SerializeField] private List<PlacedObject> connectedObjects;
-    [SerializeField]private Dictionary<string, int> maxCount;
+    [SerializeField] private Dictionary<string, int> maxCount;
     [SerializeField] float secondsPerSeason;
     //[SerializeField] int daysPerSeason;
     [SerializeField] GameObject floatingTextPrefab;
+    [SerializeField] SeasonManager seasonManager;
 
     public Season currentSeason;
     int seasonsPassed;
@@ -70,6 +71,7 @@ public class EconomyManager : MonoBehaviour
             {
                 timePassedInSeconds = 0;
                 AdvanceSeason();
+                seasonManager.ChangeSeason(currentSeason);
                 PauseTime();
 
                 if (seasonsPassed >= 4)
