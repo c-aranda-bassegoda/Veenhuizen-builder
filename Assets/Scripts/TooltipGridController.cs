836dd4ce09ca2f5059f4f6d6a95e781f633ac055
Pepijn <pepijn.luchtmeijer@gmail.com>
2026-01-22 00:46:16 +0100

    fixed tooltip flicker

--- Assets/Scripts/TooltipGridController.cs
+++ Assets/Scripts/TooltipGridController.cs
@@ -11,7 +11,9 @@ public class TooltipGridController : MonoBehaviour
     [SerializeField] private float delay = 1f;
 
     private Coroutine hoverCoroutine;
-    private TooltipBuildingData currentData;
+    [SerializeField] private TooltipBuildingData currentData, previousData;
+    [SerializeField] bool exiting, tooltipShowing;
+    [SerializeField] float exitBuffer;
 
     private void Start()
     {
@@ -33,26 +35,63 @@ public class TooltipGridController : MonoBehaviour
     private void HandleHover(Vector3 worldPos)
     {
         GridObject obj = grid.GetGridObj(worldPos);
-        if (obj == null) return;
+        if (obj == null)
+        {
+            currentData = null;
+            return;
+        }
 
         var placedObject = obj.GetPlacedObject();
-        if (placedObject == null) return;
-
+        if (placedObject == null)
+        {
+            currentData = null;
+            return;
+        }
         var scriptableObject = placedObject.GetScriptableObject();
-        if (scriptableObject == null) return;
+        if (scriptableObject == null)
+        {
+            currentData = null;
+            return;
+        }
 
         TooltipBuildingData data = scriptableObject.GetData();
-        if (data == null) return;
+        if (data == null)
+        {
+            currentData = null;
+            return;
+        }
+        if (data.GetHeader() == "Empty")
+        {
+            placedObject = placedObject.parent;
+            if(placedObject == null)
+            {
+                currentData = null;
+                return;
+            }
+            scriptableObject = placedObject.GetScriptableObject();
+            if (scriptableObject == null)
+            {
+                currentData = null;
+                return;
+            }
+            data = scriptableObject.GetData();
+            if (data == null)
+            {
+                currentData = null;
+                return;
+            }
+        }
 
-        if (data == currentData) return;
+        if (IsSameData(data, currentData)) return;
+        if (IsSameData(data, previousData) && previousData != null) return;
 
         currentData = data;
-        RestartHover();
+        if(!exiting) RestartHover();
     }
 
     private void RestartHover()
     {
-        StopHover();
+        StartCoroutine(StopHover());
         Debug.Log("starting hover coroutine");
         hoverCoroutine = StartCoroutine(ShowAfterDelay());
     }
@@ -60,24 +99,59 @@ public class TooltipGridController : MonoBehaviour
     private IEnumerator ShowAfterDelay()
     {
         yield return new WaitForSeconds(delay);
-        TooltipSystem.instance.Show(currentData.GetBody(), currentData.happinessCount, currentData.controlCount, currentData.peopleCount, currentData.cost, currentData.GetHeader());
+        if(currentData != null)
+        {
+            TooltipSystem.instance.Show(currentData.GetBody(), currentData.happinessCount, currentData.controlCount, currentData.peopleCount, currentData.cost, currentData.GetHeader());
+            tooltipShowing = true;
+        }
     }
 
     private void HandleExit()
     {
-        StopHover();
-        Debug.Log("stopping hover coroutine");
-        TooltipSystem.Hide();
-        currentData = null;
+        StartCoroutine(StopHover());
     }
 
-    private void StopHover()
+    private IEnumerator StopHover()
     {
-        if (hoverCoroutine != null)
+        if (!exiting && tooltipShowing)
         {
-            StopCoroutine(hoverCoroutine);
-            hoverCoroutine = null;
+            exiting = true;
+            previousData = currentData;
+            yield return new WaitForSeconds(exitBuffer);
+
+            if (!IsSameData(previousData, currentData))
+            {
+                if (hoverCoroutine != null)
+                {
+                    StopCoroutine(hoverCoroutine);
+                    hoverCoroutine = null;
+                }
+                TooltipSystem.Hide();
+                tooltipShowing = false;
+            }
+            
+            currentData = null;
+            previousData = null;
+            exiting = false;
         }
+    }
+
+    bool IsSameData(TooltipBuildingData data1, TooltipBuildingData data2)
+    {
+        if (data1 == null && data2 == null) return false;
+        if (data1 == null) return false;
+        if (data2 == null) return false;
+
+        bool isSameData = true;
+
+        if(data1.controlCount != data2.controlCount) isSameData = false;
+        if(data1.peopleCount != data2.peopleCount) isSameData = false;
+        if(data1.happinessCount != data2.happinessCount) isSameData = false;
+        if(data1.cost != data2.cost) isSameData = false;
+        if(data1.GetHeader() != data2.GetHeader()) isSameData = false;
+
+        return isSameData;
+
     }
 }
 
